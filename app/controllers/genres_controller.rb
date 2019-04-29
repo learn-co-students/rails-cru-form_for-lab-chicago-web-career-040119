5ca6bd1447d9c2de149ac0769a212d707487ed14
github username <github email address>
2019-04-29 18:27:10 -0500

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,5 +1,5 @@
 class GenresController < ApplicationController
-  
+
   def index
     @genres = Genre.all
   end
@@ -27,13 +27,13 @@ class GenresController < ApplicationController
 
   def update
     @genre = Genre.find(params[:id])
-    @genre = Genre.update(genre_params(:name))
+    @genre = Genre.update(genre_params)
     redirect_to genres_path
   end
 
   private
 
-  def genre_params(*args)
+  def genre_params
     params.require(:genre).permit(:name)
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 # require 'pry'
 class SongsController < ApplicationController
-  
+
   def index
     @songs = Song.all
   end
@@ -18,20 +18,23 @@ class SongsController < ApplicationController
 
   def edit
     @song = Song.find(params[:id])
-    @song.update(song_params(:name, :artist_id, :genre_id))
+    @song.update(song_params)
     redirect_to songs_path
   end
 
   def create
     @song = Song.new(song_params)
     if @song.save
-      redirect_to songs_path
+      redirect_to song_path(@song)
     else
       render :new
     end
   end
 
   def update
+    @song = Song.find(params[:id])
+    @song = Song.update(song_params)
+    redirect_to songs_path
   end
 
   private
